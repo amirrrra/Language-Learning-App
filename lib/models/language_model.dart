b79c5c4f41cd2d6f3c4dda086dfd0f49a7399f81
Amira Tarek <amirat737@gmail.com>
2024-05-20 00:24:34 +0300

    create language model

---
+++ lib/models/language_model.dart
@@ -0,0 +1,13 @@
+class LanguageModel {
+  final String sound;
+  final String enName;
+  final String jpName;
+  final String img;
+
+  const LanguageModel({
+    required this.enName,
+    required this.jpName,
+    required this.img,
+    required this.sound,
+  });
+}
